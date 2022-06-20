03557f3fbf67fe8ae2bbe1cbfd2464e5efcb0902
myka1 <msmykolas@gmail.com>
2022-06-20 14:29:11 +0300

    Update RepeatIsBad.java

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -21,7 +21,6 @@ public class RepeatIsBad {
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
-        System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
-   
+        System.out.println("writing the same code doesn't have much impact, and it's also time consuming");   
     }
 }
